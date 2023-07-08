8c850eae3ef15e1bd9843f928445810bfec7c2c9
f190915-Zain <f190915@cfd.nu.edu.pk>
2023-07-08 22:15:40 +0500

    Login Functionality Added

---
+++ dcrm/website/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+
+
+def home(request):
+    # check to see if logging in
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request, "You Have Been Logged in")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an Error!")
+            return redirect('home')
+    else:           
+        return render(request, 'home.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You Have Been Logged out...")
+    return redirect('home')
